b3571e0a2ee5080707bacde4ba734033896effd9
Tom Reed <t.reed1104@gmail.com>
2020-11-23 15:29:27 +0000

    Amended index component text

--- app/src/components/Index.vue
+++ app/src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <h1 class="page-title">Webpack Template</h1>
-        <p>A project template for web projects using Webpack, Vue.js, SCSS and Bootstrap</p>
+        <p>A project template for web projects using Webpack, Babel, Vue, Sass and Bootstrap. The template supports both Javascript and Typescript as usable languages.</p>
     </div>
 </template>
 
