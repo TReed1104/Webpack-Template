f34865938416f5ea6aad185b31fe66d88187f807
Tom Reed <t.reed1104@gmail.com>
2020-11-25 12:00:18 +0000

    Imported shallowMount to the test script too

--- app/src/tests/App.test.js
+++ app/src/tests/App.test.js
@@ -1,5 +1,5 @@
 // Testing core packages
-import { mount, createLocalVue } from '@vue/test-utils';
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import router from '../router/index.js';
 import Vuex from 'vuex';
