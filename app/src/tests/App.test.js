deb18a5231ecd5d1adc66fbe39517a1a97b6a781
Tom Reed <t.reed1104@gmail.com>
2020-11-25 11:58:08 +0000

    Component imports for the tests

--- app/src/tests/App.test.js
+++ app/src/tests/App.test.js
@@ -4,3 +4,8 @@ import VueRouter from 'vue-router';
 import router from '../router/index.js';
 import Vuex from 'vuex';
 
+// Components
+import App from '../components/App.vue';
+import Home from '../components/Home.vue';
+import JavascriptTemplate from '../components/TemplateJavascript.vue';
+import TypescriptTemplate from "../components/TemplateTypescript.vue";
