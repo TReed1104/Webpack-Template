6a44f309dfcff8e4664f671c7fb59327e546abbf
Tom Reed <t.reed1104@gmail.com>
2020-11-25 11:55:48 +0000

    Amended our router import to be clear

--- app/src/tests/App.test.js
+++ app/src/tests/App.test.js
@@ -1,5 +1,6 @@
 // Testing core packages
 import { mount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
+import router from '../router/index.js';
 import Vuex from 'vuex';
 
